bff96639496080eb5724b9ed57f8339c9e8cafaf
GanstaKingofSA <azarieldc@gmail.com>
2021-10-09 02:48:15 +0000

    APK finally works. RPA testing still needed

--- python-packages/ost_backend.py
+++ python-packages/ost_backend.py
@@ -2,5 +2,7 @@
 import renpy
 import ost_loader
 
+renpy.loader.SubFile = ost_loader.AltSubFile
+
 def file(fn):
     return ost_loader.load(fn)
--- python-packages/ost_loader.py
+++ python-packages/ost_loader.py
@@ -2,10 +2,14 @@
 import os
 import renpy
 import re
-#modded android/apk.py imports
-import struct
-import zipfile
-import io
+import unicodedata
+
+# Files on disk should be checked before archives. Otherwise, among
+# other things, using a new version of bytecode.rpyb will break.
+archives = [ ]
+
+# A map from lower-case filename to regular-case filename.
+lower_map = { }
 
 class AltSubFile(object):
 
@@ -151,6 +155,8 @@ class AltSubFile(object):
         self.f.seek(offset + self.base)
 
     def peek(self, offset):
+        if self.f is None:
+            self.open()
         return self.f.read()
 
     def tell(self):
@@ -163,46 +169,6 @@ class AltSubFile(object):
 
     def write(self, s):
         raise Exception("Write not supported by SubFile/AltSubFile")
-
-class AltAPK(object):
-
-    def __init__(self, apk=None, prefix='assets/'):
-        if apk is None:
-            apk = os.environ['ANDROID_APK']
-            print 'Opening APK %r' % apk
-        self.apk = apk
-        self.zf = zipfile.ZipFile(apk, 'r')
-        self.info = {}
-        for i in self.zf.infolist():
-            fn = i.filename
-            if not fn.startswith(prefix):
-                continue
-            fn = fn[len(prefix):]
-            self.info[fn] = i
-
-        f = file(self.apk, 'rb')
-        self.offset = {}
-        import time
-        start = time.time()
-        for fn, info in self.info.items():
-            f.seek(info.header_offset)
-            h = struct.unpack(zipfile.structFileHeader, f.read(zipfile.sizeFileHeader))
-            self.offset[fn] = info.header_offset + zipfile.sizeFileHeader + h[zipfile._FH_FILENAME_LENGTH] + h[zipfile._FH_EXTRA_FIELD_LENGTH]
-
-        f.close()
-        return
-
-    def list(self):
-        return sorted(self.info)
-
-    def open(self, fn):
-        if fn not in self.info:
-            raise IOError(('{0} not found in apk.').format(fn))
-        info = self.info[fn]
-        if info.compress_type == zipfile.ZIP_STORED:
-            return AltSubFile(self.apk, self.offset[fn], info.file_size)
-        return io.BytesIO(self.zf.read(info))
-
 
 def get_prefixes(tl=True):
     """
@@ -225,7 +191,6 @@ def get_prefixes(tl=True):
 
     return rv
 
-
 def load(name, tl=True):
 
     if renpy.display.predict.predicting: # @UndefinedVariable
@@ -239,8 +204,215 @@ def load(name, tl=True):
     name = re.sub(r'/+', '/', name).lstrip('/')
 
     for p in get_prefixes(tl):
-        rv = renpy.loader.load_core(p + name)
+        rv = load_core(p + name)
         if rv is not None:
             return rv
 
     raise IOError("Couldn't find file '%s'." % name)
+
+# A list of callbacks to open an open python file object of the given type.
+file_open_callbacks = [ ]
+
+def load_core(name):
+    """
+    Returns an open python file object of the given type.
+    """
+
+    name = lower_map.get(unicodedata.normalize('NFC', name.lower()), name)
+
+    for i in file_open_callbacks:
+        rv = i(name)
+        if rv is not None:
+            return rv
+
+    return None
+
+def load_from_file_open_callback(name):
+    """
+    Returns an open python file object of the given type from the file open callback.
+    """
+
+    if renpy.config.file_open_callback:
+        return renpy.config.file_open_callback(name)
+
+    return None
+
+
+file_open_callbacks.append(load_from_file_open_callback)
+
+
+def load_from_filesystem(name):
+    """
+    Returns an open python file object of the given type from the filesystem.
+    """
+
+    if not renpy.config.force_archives:
+        try:
+            fn = transfn(name)
+            return open_file(fn, "rb")
+        except:
+            pass
+
+    return None
+
+
+file_open_callbacks.append(load_from_filesystem)
+
+
+def load_from_apk(name):
+    """
+    Returns an open python file object of the given type from the apk.
+    """
+
+    for apk in apks:
+        prefixed_name = "/".join("x-" + i for i in name.split("/"))
+
+        try:
+            return apk.open(prefixed_name)
+        except IOError:
+            pass
+
+    return None
+
+
+if renpy.android:
+    file_open_callbacks.append(load_from_apk)
+
+
+def load_from_archive(name):
+    """
+    Returns an open python file object of the given type from an archive file.
+    """
+
+    for prefix, index in archives:
+        if not name in index:
+            continue
+
+        afn = transfn(prefix)
+
+        data = [ ]
+
+        # Direct path.
+        if len(index[name]) == 1:
+
+            t = index[name][0]
+            if len(t) == 2:
+                offset, dlen = t
+                start = b''
+            else:
+                offset, dlen, start = t
+
+            rv = SubFile(afn, offset, dlen, start)
+
+        # Compatibility path.
+        else:
+            with open(afn, "rb") as f:
+                for offset, dlen in index[name]:
+                    f.seek(offset)
+                    data.append(f.read(dlen))
+
+                rv = io.BytesIO(b''.join(data))
+
+        return rv
+
+    return None
+
+
+file_open_callbacks.append(load_from_archive)
+
+def get_prefixes(tl=True):
+    """
+    Returns a list of prefixes to search for files.
+    """
+
+    rv = [ ]
+
+    if tl:
+        language = renpy.game.preferences.language # @UndefinedVariable
+    else:
+        language = None
+
+    for prefix in renpy.config.search_prefixes:
+
+        if language is not None:
+            rv.append(renpy.config.tl_directory + "/" + language + "/" + prefix)
+
+        rv.append(prefix)
+
+    return rv
+
+def loadable_core(name):
+    """
+    Returns True if the name is loadable with load, False if it is not.
+    """
+
+    name = lower_map.get(unicodedata.normalize('NFC', name.lower()), name)
+
+    if name in loadable_cache:
+        return loadable_cache[name]
+
+    try:
+        transfn(name)
+        loadable_cache[name] = True
+        return True
+    except:
+        pass
+
+    for apk in apks:
+        prefixed_name = "/".join("x-" + i for i in name.split("/"))
+        if prefixed_name in apk.info:
+            loadable_cache[name] = True
+            return True
+
+    for _prefix, index in archives:
+        if name in index:
+            loadable_cache[name] = True
+            return True
+
+    if name in remote_files:
+        loadable_cache[name] = True
+        return name
+
+    loadable_cache[name] = False
+    return False
+
+
+def loadable(name):
+
+    name = name.lstrip('/')
+
+    if (renpy.config.loadable_callback is not None) and renpy.config.loadable_callback(name):
+        return True
+
+    for p in get_prefixes():
+        if loadable_core(p + name):
+            return True
+
+    return False
+
+
+def transfn(name):
+    """
+    Tries to translate the name to a file that exists in one of the
+    searched directories.
+    """
+
+    name = name.lstrip('/')
+
+    if renpy.config.reject_backslash and "\\" in name:
+        raise Exception("Backslash in filename, use '/' instead: %r" % name)
+
+    name = lower_map.get(unicodedata.normalize('NFC', name.lower()), name)
+
+    if isinstance(name, bytes):
+        name = name.decode("utf-8")
+
+    for d in renpy.config.searchpath:
+        fn = os.path.join(renpy.config.basedir, d, name)
+
+        add_auto(fn)
+
+        if os.path.isfile(fn):
+            return fn
+
+    raise Exception("Couldn't find file '%s'." % name)
